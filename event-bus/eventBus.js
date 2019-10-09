b30041403081b755d744ae5093d538e6f1ecd01c
pupysheva <anime-manga@bk.ru>
2019-10-09 21:54:25 +0300

    Первый вариант Второй практики по шине

--- event-bus/eventBus.js
+++ event-bus/eventBus.js
@@ -29,22 +29,56 @@ let eventBus = {
 }
 
 
-eventBus.events["LOG"] = "eventType1";
-eventBus.events["INC"] = "eventType2";
+function moduleD() {
+    eventBus.events["LOG"] = "eventType1";
+    eventBus.events["INC"] = "eventType2";
 
 
 
-eventBus.accept((arg) => { console.log(arg) }, "eventType1")
-eventBus.accept(() => {
-    console.log(eventBus.storage.catCounter)
-}, "eventType1")
-eventBus.accept(() => { return eventBus.storage["catCounter"] += 1 }, "eventType2")
+    eventBus.accept((arg) => { console.log(arg) }, "eventType1")
+    eventBus.accept(() => {
+        console.log(eventBus.storage.catCounter)
+    }, "eventType1")
+    eventBus.accept(() => { return eventBus.storage["catCounter"] += 1 }, "eventType2")
 
 
-//Вызывают события LOG и INC
-eventBus.event("INC")
-eventBus.event("LOG", "Caaaaaaats!))")
+    //Вызывают события LOG и INC
+    eventBus.event("INC")
+    eventBus.event("LOG", "Caaaaaaats!))")
+}
+
+function moduleE() {
+    eventBus.storage["cats"] = [];
+    for (i = 0; i <= 30000; i++) {
+        let x = i;
+        //setTimeout позволяет вызвать функцию один раз через определённый интервал времени.
+        setTimeout(function () {
+            let cat = { name: "Cat " + x };
+            eventBus.storage["cats"].push(cat);
+            console.log(eventBus.storage["cats"][x]);
+        }, 100);
+    }
+    //console.log(eventBus.storage["cats"]);
+
+    /*function loger() {
+        console.log("I am in log()");
+        cats = eventBus.storage["cats"];
+        for (i = 0; i < cats.length; i++) {
+            let cat = cats[i];
+            (function (cat) {
+                setTimeout(function () {
+                    console.log(cat.name);
+                }, 100);
+            }(cat));
+        }
+    }*/
+}
 
 
+moduleD();
+moduleE();
+//setTimeout(console.log("ALL cats: ",eventBus.storage["cats"]),1000000);
 
-setTimeout(() => console.log('exit...'), 100000000);
+
+//for visual studio
+//setTimeout(() => console.log('exit...'), 100000000);
