90152505ada02cb0ea701afa8d8a0a6a7c790681
pupysheva <anime-manga@bk.ru>
2019-10-10 13:35:59 +0300

    Второе практическое задание по шине

--- event-bus/eventBus.js
+++ event-bus/eventBus.js
@@ -1,4 +1,8 @@
-/*MyFunction();*/
+//Lинамический импорт
+const {
+    performance,
+} = require("perf_hooks");
+
 let eventBus = {
     events: {}, //eName:eType
     listeners: {}, //eType:массив функций
@@ -22,9 +26,9 @@ let eventBus = {
         this.listeners[eventType].push(listener);//Добавляем в конец массива функцию
     },
 
-
     storage: {
         catCounter: 0,
+        cats: [{ name: "Cat  -1))" }],
     },
 }
 
@@ -32,6 +36,7 @@ let eventBus = {
 function moduleD() {
     eventBus.events["LOG"] = "eventType1";
     eventBus.events["INC"] = "eventType2";
+    eventBus.events["ADD"] = "eventType3";
 
 
 
@@ -40,7 +45,12 @@ function moduleD() {
         console.log(eventBus.storage.catCounter)
     }, "eventType1")
     eventBus.accept(() => { return eventBus.storage["catCounter"] += 1 }, "eventType2")
-
+    eventBus.accept((cat) => {
+        if (eventBus.storage["cats"] === undefined)
+            eventBus.storage["cats"] = [];
+        eventBus.storage["cats"].push(cat);
+        return eventBus.storage["cats"].length;
+    }, "eventType3")
 
     //Вызывают события LOG и INC
     eventBus.event("INC")
@@ -48,37 +58,41 @@ function moduleD() {
 }
 
 function moduleE() {
-    eventBus.storage["cats"] = [];
-    for (i = 0; i <= 30000; i++) {
-        let x = i;
+    let x;
+    for (let i = 0; i <30000; i++) {
         //setTimeout позволяет вызвать функцию один раз через определённый интервал времени.
+        //setTimeout(sayHi, 1000, "Привет", "Джон"); имя функции, задержка перед запуском в миллисекундах (1000 мс = 1 с), аргументы для функции sayHi
+        //setTimeout ожидает ссылку на функцию. Вызывать функцию не надо
         setTimeout(function () {
-            let cat = { name: "Cat " + x };
-            eventBus.storage["cats"].push(cat);
-            console.log(eventBus.storage["cats"][x]);
+            x = { name: "Cat " + i }
+            console.log(x);
+            eventBus.event("ADD", x);
         }, 100);
     }
-    //console.log(eventBus.storage["cats"]);
-
-    /*function loger() {
-        console.log("I am in log()");
-        cats = eventBus.storage["cats"];
-        for (i = 0; i < cats.length; i++) {
-            let cat = cats[i];
-            (function (cat) {
-                setTimeout(function () {
-                    console.log(cat.name);
-                }, 100);
-            }(cat));
-        }
-    }*/
 }
 
 
 moduleD();
-moduleE();
-//setTimeout(console.log("ALL cats: ",eventBus.storage["cats"]),1000000);
+console.log("Storage start: ", eventBus.storage);
+
+let time1 = performance.now();
+moduleE(eventBus);
+let time2 = performance.now();
+console.log("Time :", time2 - time1);
 
 
-//for visual studio
-//setTimeout(() => console.log('exit...'), 100000000);
+setTimeout(() => { console.log("Storage result: ", eventBus.storage); console.log("Time :", time2 - time1) } , 3000);
+
+
+/*function loger() {
+    console.log("I am in log()");
+    cats = eventBus.storage["cats"];
+    for (i = 0; i < cats.length; i++) {
+        let cat = cats[i];
+        (function (cat) {
+            setTimeout(function () {
+                console.log(cat.name);
+            }, 100);
+        }(cat));
+    }
+}*/
