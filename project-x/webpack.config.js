38a6d2ef3fba3d9650242189cd6fd496f64097a0
SG_Muwa <motherlode.muwa@gmail.com>
2019-11-13 10:15:43 +0300

    Добавлен комментарий __dirname.

--- project-x/webpack.config.js
+++ project-x/webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path'); // import path from 'path';
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+	filename: 'main.js',
+	// __dirname - получить путь до файла.
     path: path.resolve(__dirname, 'dist'),
   },
 };
